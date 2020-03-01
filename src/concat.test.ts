340a0c6165a3035bc1601a6709cb67b757e88fbc
nicholas husher <nhusher@indigoag.com>
2020-03-01 12:47:52 -0500

    Expand concat tests

--- src/concat.test.ts
+++ src/concat.test.ts
@@ -8,4 +8,32 @@ describe('concat', () => {
     const output = await sequence<number[], number>(concat, input)
     assert.deepEqual(output, [ 1, 2, 3, 4, 5, 6 ])
   })
+
+  it('should allow nested items to be iterables', async () => {
+    function * range (start: number, end: number) {
+      for (let i = start; i < end + 1; i += 1) {
+        yield i
+      }
+    }
+
+    const input: Generator<number>[] = [range(1, 5), range(6, 10), range(11, 15)]
+    const output = await sequence(concat, input)
+    assert.deepEqual(output, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+  })
+
+  it('should allow nested items to be async iterables', async () => {
+    function wait (ms: number) {
+      return new Promise(resolve => setTimeout(resolve, ms))
+    }
+    async function * range (start: number, end: number) {
+      for (let i = start; i < end + 1; i += 1) {
+        await wait(0)
+        yield i
+      }
+    }
+
+    const input: AsyncGenerator<number>[] = [range(1, 5), range(6, 10), range(11, 15)]
+    const output = await sequence(concat, input)
+    assert.deepEqual(output, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+  })
 })
