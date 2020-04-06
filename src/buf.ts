4bd9b4b99ee54b28e87ec8ef4c66f71845f25c65
nicholas husher <nhusher@indigoag.com>
2020-04-06 17:14:39 -0400

    Add experimental buffer step

---
+++ src/buf.ts
@@ -0,0 +1,130 @@
+import { Step } from './types'
+
+const MAX_WAITING_PUTS = 128;
+
+export class Buf<T> implements AsyncIterable<T> {
+  closed: boolean
+  size: number
+  items: T[]
+  waitingTakes: ((it?: T) => void)[]
+  waitingPuts: ((pushed?: boolean) => T)[]
+
+  constructor (size: number) {
+    this.closed = false
+    this.size = size
+    this.items = []
+    this.waitingTakes = []
+    this.waitingPuts = []
+  }
+
+  put (it: T): Promise<boolean> {
+    if (this.closed) {
+      return Promise.resolve(false)
+    } else if (this.waitingTakes[0]) {
+      this.waitingTakes.shift()!(it)
+      return Promise.resolve(true)
+    } else if (this.full) {
+      if (this.waitingPuts.length > MAX_WAITING_PUTS)
+        throw new RangeError(`Too many waiting puts`)
+
+      return new Promise((resolve) => {
+        this.waitingPuts.push((pushed = true) => {
+          resolve(pushed);
+          return it;
+        })
+      })
+    } else {
+      this.items.push(it)
+      return Promise.resolve(true)
+    }
+  }
+
+  take (): Promise<T | undefined> {
+    if (this.closed) {
+      return Promise.resolve(undefined)
+    } else if (this.items.length) {
+      return Promise.resolve(this.items.shift())
+    } else if (this.waitingPuts) {
+      return Promise.resolve(this.waitingPuts.shift()!())
+    } else {
+      return new Promise((resolve, _reject) => {
+        this.waitingTakes.push((it?: T) => resolve(it))
+      })
+    }
+  }
+
+  close (it?: T) {
+    this.closed = true
+    this.waitingPuts.forEach(put => put(false))
+    this.waitingTakes.forEach(take => take(undefined))
+  }
+
+  get full (): boolean {
+    return this.items.length >= this.size
+  }
+
+  [Symbol.asyncIterator]() {
+    const self = this;
+    return {
+      async next(): Promise<IteratorResult<T, undefined>> {
+        if (self.closed && self.items.length === 0) {
+          return Promise.resolve({ value: undefined, done: true })
+        } else {
+          const value = await self.take()
+          return { value, done: value === undefined } as IteratorResult<T, undefined>
+        }
+      }
+    }
+  }
+}
+
+function buf<T> (size: number, fill: (buf: Buf<T>, it: Step<T>) => void) {
+  return async function * buffer (it: Step<T>) {
+    const buf = new Buf<T>(size)
+    fill(buf, it)
+
+    for await (let i of buf) {
+      yield i
+    }
+
+    buf.close()
+  }
+}
+
+async function haltingFill<T> (buf: Buf<T>, it: Step<T>) {
+  for await (let i of it) {
+    if (buf.closed) break
+    await buf.put(i)
+  }
+  buf.close()
+}
+
+export function buffer<T> (size: number) {
+  return buf<T>(size, haltingFill)
+}
+
+async function droppingFill<T> (buf: Buf<T>, it: Step<T>) {
+  for await (let i of it) {
+    if (buf.closed) break
+    if (buf.full) continue // ignore new
+    await buf.put(i)
+  }
+  buf.close()
+}
+
+export function droppingBuffer<T> (size: number) {
+  return buf<T>(size, droppingFill)
+}
+
+async function windowingFill<T> (buf: Buf<T>, it: Step<T>) {
+  for await (let i of it) {
+    if (buf.closed) break
+    if (buf.full) await buf.take() // drop the last
+    await buf.put(i) // add new
+  }
+  buf.close()
+}
+
+export function windowedBuffer<T> (size: number) {
+  return buf<T>(size, windowingFill)
+}
