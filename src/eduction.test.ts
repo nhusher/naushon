033b9b1341ebff813e26456cf4be8f5dc556755a
nicholas husher <nhusher@indigoag.com>
2020-04-25 19:35:08 -0400

    Test for errors in eduction

--- src/eduction.test.ts
+++ src/eduction.test.ts
@@ -1,16 +1,30 @@
 import assert from 'assert'
 import { eduction } from './eduction'
-import { map } from './map'
 import { isAsyncIterator } from './iterator'
+import { identity } from './identity'
+import { map } from './map'
 
 describe('eduction', () => {
   it('should yield an async generator from an array', async () => {
-    const it = eduction(map(v => v), [1, 2, 3, 4, 5])
+    const it = eduction(identity, [1, 2, 3, 4, 5])
     assert(isAsyncIterator(it))
     const output = []
     for await (let i of it) {
       output.push(i)
     }
     assert.deepEqual(output, [1, 2, 3, 4, 5])
+  })
+  it('should do something when it throws', async () => {
+    const it = eduction(map(() => {
+      throw new Error(`An error`)
+    }), [1, 2, 3])
+
+    try {
+      for await (let i of it) {
+        console.log(i)
+      }
+    } catch (e) {
+      console.log(e)
+    }
   })
 })
